2cbdf0be3d6e230671f9d69f5782d7df2137cb4e
Prateek-S004 <psachhu00799@gmail.com>
2024-11-29 13:19:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section => {
+  section.addEventListener('mouseenter', () => {
+    section.style.transform = 'scale(1.05)';
+  });
+
+  section.addEventListener('mouseleave', () => {
+    section.style.transform = 'scale(1)';
+  });
+});
