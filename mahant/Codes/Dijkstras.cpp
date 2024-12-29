18bbf823f2db2c920c004a95fc4b4e92382bcfaa
Mahant-D-H <mahanthatapaki@gmail.com>
2024-12-29 05:35:09 +0530

    Update Dijkstras.cpp

--- mahant/Codes/Dijkstras.cpp
+++ mahant/Codes/Dijkstras.cpp
@@ -8,36 +8,46 @@
 #include <algorithm>
 using namespace std;
 
-class Dijistras{
+class Dijkstra{
     public:
-        vector<int> V, vis, dist, path;
-        vector<vector<int>> cost_matrix;
-        int n, src, relax_e, cnt = 0;
-
+        vector<int> V, vis, path;
+        vector<float> dist;
+        vector<vector<float>> cost_matrix;
+        int n = 18, src, relax_e, cnt = 0;
+        // compile time initializing number of vertices = 18
         void read_input();
         void initialize();
         void dijistra_algo();
         void display(int x);
-
-
 };
 
-void Dijistras::read_input(){
-    cout << "Enter number of vertices : ";
-    cin >> n;
+void Dijkstra::read_input(){
     for(int i = 0 ; i < n ; i++){
         V.push_back(i);
     }
-    cost_matrix.resize(n, vector<int>(n));
-    cout << "Enter the cost matrix : " << endl;
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            cin >> cost_matrix[i][j];
-        }
+
+    cost_matrix.resize(n);
+    for(int i = 0; i < n; i++){
+        vector<float> temp(n);
+        fill(temp.begin(), temp.end(), 999.9);
+        
+        cost_matrix[i] = temp;            
+        cost_matrix[i][i] = 0.0;
     }
+
+    ifstream file("city_num.txt");
+    if(!file){
+        cout << "File not found." << endl;
+    }
+    int u, v;
+    float w;
+    while(file >> u >> v >> w){
+        cost_matrix[u][v] = w;
+    }
+    file.close();
 }
 
-void Dijistras::initialize(){
+void Dijkstra::initialize(){
     cout << "Enter source : ";
     cin >> src;
 
@@ -60,7 +70,7 @@ void Dijistras::initialize(){
     relax_e = src;
 }
 
-void Dijistras::dijistra_algo(){
+void Dijkstra::dijistra_algo(){
     int min_dist_index = 0;
     for(int i = 1 ; i < n ; i++){
         if(vis[i] == 0){
@@ -76,7 +86,7 @@ void Dijistras::dijistra_algo(){
     for(int i = 0; i < n ; i++){
         if(vis[i] == 0){
             // dist[not visited] = min(dist[not visited], dist[relaxed edge] + cost_matrix[relaxed edge][not visited])
-            if(dist[i] > dist[relax_e] + cost_matrix[relax_e][i] && dist[relax_e] < 999){
+            if(dist[i] > dist[relax_e] + cost_matrix[relax_e][i] && dist[relax_e] < 999.9){
                 path[i] = relax_e;
                 dist[i] = dist[relax_e] + cost_matrix[relax_e][i];
             }
@@ -84,7 +94,7 @@ void Dijistras::dijistra_algo(){
     }
 }
 
-void Dijistras::display(int x){
+void Dijkstra::display(int x){
     vector<int> ans;
     ans.push_back(x);
 
@@ -103,15 +113,15 @@ void Dijistras::display(int x){
 }
 
 int main(){
-    Dijistras D;
+    Dijkstra D;
     D.read_input();
     D.initialize();
 
-    for(int i = 0; i < D.n - 1; i++){
+    for(int i = 0; i < D.n ; i++){
         D.dijistra_algo();
     }
 
-    for(int i = 0; i < D.n - 1; i++ ){
+    for(int i = 0; i < D.n ; i++ ){
         D.display(i);
     }
     return 0;
